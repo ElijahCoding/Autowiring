1b94205e6c50524195e230bb0e4d0789b6575390
Elijah <elijahsharper1@gmail.com>
2018-03-14 11:33:31 +0300

    container built up and set up Exceptions

--- app/Container/Container.php
+++ app/Container/Container.php
@@ -4,5 +4,23 @@ namespace App\Container;
 
 class Container
 {
-  
+  protected $items = [];
+
+  public function set($name, callable $closure)
+  {
+    $this->items[$name] = $closure;
+  }
+
+  public function has($name)
+  {
+    return isset($this->items[$name]);
+  }
+
+  public function get($name)
+  {
+    if (!$this->has($name)) {
+      throw new NotFoundException;
+    }
+    return $this->items[$name]();
+  }
 }
