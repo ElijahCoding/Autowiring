a831b91c15e0cb8114b7f0cac6dcdde054364b33
Elijah <elijahsharper1@gmail.com>
2018-03-14 11:08:30 +0300

    configuration set up

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -4,5 +4,68 @@ namespace App\Config;
 
 class Config
 {
-  
+  protected $config = [
+
+    'app' => [
+      'name' => 'autowiring'
+    ],
+
+    'db' => [
+      'host' => '127.0.0.1',
+      'database' => 'autowiring',
+      'username' => 'root',
+      'password' => 'password',
+    ]
+  ];
+
+  protected $cache = [];
+
+  public function get($key, $default = null)
+  {
+    if ($this->existsInCache($key)) {
+      return $this->fromCache($key);
+    }
+
+    return $this->addToCache(
+      $key, $this->extractFromConfig($key) ?? $default
+    );
+  }
+
+  protected function extractFromConfig($key)
+  {
+    $filtered = $this->config;
+
+    foreach (explode('.', $key) as $segment) {
+      if ($this->exists($filtered, $segment)) {
+        $filtered = $filtered[$segment];
+        continue;
+      }
+
+      return;
+    }
+
+    return $filtered;
+  }
+
+  protected function addToCache($key, $value)
+  {
+    $this->cache[$key] = $value;
+
+    return $value;
+  }
+
+  protected function existsInCache($key)
+  {
+    return isset($this->cache[$key]);
+  }
+
+  protected function fromCache($key)
+  {
+    return $this->cache[$key];
+  }
+
+  protected function exists(array $config, $key)
+  {
+    return array_key_exists($key, $config);
+  }
 }
